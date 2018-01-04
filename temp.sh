2d6004bdc3b5da077ac94a7d96db047ed5723fde
Jakub Janek <janek.jakub@gmail.com>
2018-01-04 14:06:51 +0100

    Fixed some text and added infinite loop

--- temp.sh
+++ temp.sh
@@ -6,9 +6,10 @@ function help {
 	echo "-h, --help display this help"
 	echo "-b, --bare measure temperture once."
 	echo "-l, --loop [quantity] [speed]"
+	echo "You can also loop infinitely by setting quantity to 'inf'"
 	echo "Example: "
 	echo "(./temp.sh --loop 250 10)"
-	echo "will measure 250 with 10 seconds between each measure"
+	echo "will measure 250 times with 10 seconds between each measure"
 	echo ""
 	exit
 }
@@ -48,21 +49,41 @@ function loop {
 	echo "loop has ended"
 }
 
+## Parsing empty argument
 if [[ -z $1 ]]; then
-	echo "Usage ./temp.sh [[-b, --bare; -h, --help; -t, --temp [time] [quantity]]]"
+	echo "Usage ./temp.sh [[-b, --bare; -h, --help; -t, --loop [quantity] [time between each measurement]]]"
+## Parsing "-h" or "--help" argument
 elif [[ $1 = -h ]] || [[ $1 = --help ]]; then
 	help
+## Parsing "-b" or "--bare" argument
 elif [[ $1 = -b ]] || [[ $1 = --bare ]]; then
 	temp0; temp1; temp2
 	echo $temp0", "$temp1", "$temp2
-##Code which lies here is weird I will be glad if someone will fix this thanks :)
+## Parsing loop argument
 elif [[ $1 = -l ]] || [[ $1 = --loop ]]; then
+	## If argument $2 is empty loop 10 times with 1 second between each measurement
 	if [[ -z $2 ]]; then
 		loop 10 1
+	## Infinitely loop
+	elif [[ $2 = inf ]]; then
+		## If argument $3 is empty display hint
+		if [[ -z $3 ]]; then
+			echo "You need to specify quantity (see --help) "
+		else
+			while true; do
+				temp0; temp1; temp2
+				echo $temp0", "$temp1", "$temp2
+				sleep $3
+			done
+		fi
+	## If argument $3 is empty display hint
 	elif [[ -z $3 ]]; then
 		echo "You need to specify quantity (see --help) "
+
 	else
+		## Loop if $2 is number
 		if [[ $(($2)) = $2 ]]; then
+			## Loop if $3 is a number if not loop with 10 seconds between each measurement
 			if [[ $(($3)) = $3 ]]; then
 				loop $2 $3
 			else
